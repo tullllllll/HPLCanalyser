91dff62107e713fb5fcae5b627b394304aa91df3
MrTul <tulga.van.der.velde@student.nhlstenden.com>
2025-03-16 00:50:33 +0100

    ADD: Test graph ADD: Folder for structure

--- HLPC/MainWindow.axaml.cs
+++ HLPC/MainWindow.axaml.cs
@@ -5,10 +5,11 @@ namespace HLPC;
 public partial class MainWindow : Window
 {
     public string Test { get; set; } = "test";
+    public string Greeting => "Welcome to Avalonia! This is my added text.";
+
 
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = this;
     }
 }
---
+++ HLPC/SomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+
+namespace HLPC
+{
+    public class SomeViewModel
+    {
+        public string tekst { get; set; } = "tekst"; 
+        public ISeries[] Series { get; set; } = [
+            new ColumnSeries<int>(3, 4, 2),
+            new ColumnSeries<int>(4, 2, 6),
+            new ColumnSeries<double, DiamondGeometry>(4, 3, 4)
+        ];
+    }
+}
