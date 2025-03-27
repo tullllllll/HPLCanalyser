6ce6a8828c96d0beefb688d0347800f1075115f5
Yornan04 <114227923+Yornan04@users.noreply.github.com>
2025-03-27 10:23:32 +0100

    Yoran (#2)
    * Rider
    
    rider
    
    * Database connectie
    
    * Connectie opgeschoond
    
    ---------
    
    Co-authored-by: MrTul <tulga.van.der.velde@student.nhlstenden.com>

---
+++ HLPC/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace HLPC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
